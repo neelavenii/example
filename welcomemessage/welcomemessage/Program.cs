a09dc1b957e094ae0e3cfd330104ace795ac2f2a
eswar <neelaveni.eswar04@gmail.com>
2022-03-23 17:02:49 +0530

    [Eswar]added welcome message

---
+++ welcomemessage/welcomemessage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace welcomemessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adeed welcome message");
+            Console.ReadLine();
+        }
+    }
+}
